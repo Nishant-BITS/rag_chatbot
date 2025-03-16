7eb80c7e510e26ee1e5832551eaa5d20c0e5a15c
Nishant Narayan Rout <48598347+NishantRout@users.noreply.github.com>
2025-03-16 20:49:41 +0530

    Update app.py

--- app.py
+++ app.py
@@ -36,6 +36,7 @@ from transformers import pipeline
 # Download NLTK 'punkt' resource at initialization to prevent LookupError in hosted environments
 import nltk
 nltk.download('punkt', quiet=True)
+nltk.download('punkt_tab')
 nltk.download('wordnet')
 nltk.download('omw-1.4')
 
